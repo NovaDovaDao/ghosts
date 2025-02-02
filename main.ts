afc0a9210674ea3ac5dffda4b084bbe95f68b132
krystlc <alex.valle@gmail.com>
2025-02-01 20:37:05 -0500

    fix: execute if main

--- main.ts
+++ main.ts
@@ -3,28 +3,34 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { getChatHistory, addNewMessage } from "./services/chat.service.ts";
 import { createAgent, getAgent } from "./services/agent.service..ts";
 
-const router = new Router();
-router
-  .get("/chat", async (ctx) => {
-    ctx.response.body = await getChatHistory(ctx.request);
-  })
-  .post("/chat", async (ctx) => {
-    ctx.response.body = await addNewMessage(ctx.request);
-  });
+async function main() {
+  const router = new Router();
+  router
+    .get("/chat", async (ctx) => {
+      ctx.response.body = await getChatHistory(ctx.request);
+    })
+    .post("/chat", async (ctx) => {
+      ctx.response.body = await addNewMessage(ctx.request);
+    });
 
-router
-  .get("/agent", async (ctx) => {
-    ctx.response.body = await getAgent(ctx.request);
-  })
-  .post("/agent", async (ctx) => {
-    ctx.response.body = await createAgent(ctx.request);
-  });
+  router
+    .get("/agent", async (ctx) => {
+      ctx.response.body = await getAgent(ctx.request);
+    })
+    .post("/agent", async (ctx) => {
+      ctx.response.body = await createAgent(ctx.request);
+    });
 
-const app = new Application();
-app.use(oakCors());
-app.use(router.routes());
-app.use(router.allowedMethods());
+  const app = new Application();
+  app.use(oakCors());
+  app.use(router.routes());
+  app.use(router.allowedMethods());
 
-const port = parseInt(Deno.env.get("PORT") ?? "5001");
-console.log(`👻 boo! http://localhost:${port}`);
-await app.listen({ port });
+  const port = parseInt(Deno.env.get("PORT") ?? "5001");
+  console.log(`👻 boo! http://localhost:${port}`);
+  await app.listen({ port });
+}
+
+if (import.meta.main) {
+  main();
+}
