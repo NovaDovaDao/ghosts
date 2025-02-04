746dffe7da11f28bfaf3a451014b75e444431946
krystlc <alex.valle@gmail.com>
2025-02-03 23:11:37 -0500

    feat: get agent by id

--- services/agent.service.ts
+++ services/agent.service.ts
@@ -1,14 +1,10 @@
-import { Context, Request } from "https://deno.land/x/oak@v17.1.4/mod.ts";
-import { getUserIdFromRequest } from "./auth.service.ts";
+import { Context, RouterContext } from "https://deno.land/x/oak@v17.1.4/mod.ts";
 import { checkAnonReqCount } from "./redis.service.ts";
 import { Status } from "https://deno.land/x/oak@v17.1.4/deps.ts";
 
 const kv = await Deno.openKv();
-const getMainConfigKey = (userId: string) => [
-  "agent-configuration",
-  userId,
-  "main",
-];
+const AGENT_CONFIG = Symbol("agent-config");
+const USER_AGENTS = Symbol("user-agents");
 
 export const getAgent = async (ctx: Context) => {
   let userId = ctx.state.userId;
@@ -18,12 +14,36 @@ export const getAgent = async (ctx: Context) => {
     await checkAnonReqCount(ctx.request.ip);
   }
 
-  const agent = await kv.get<AgentConfiguration>(getMainConfigKey(userId));
+  const list = kv.list<AgentConfiguration>({ prefix: [AGENT_CONFIG, userId] });
+  const agents: AgentConfiguration[] = [];
+
+  for await (const item of list) {
+    agents.push(item.value);
+  }
+  const [firstAgent] = agents;
+
+  ctx.response.body = firstAgent;
+};
+
+export const getAgentById = async (ctx: Context) => {
+  let userId = ctx.state.userId;
+  console.log("is n8n request?", ctx.request.headers.get("Authorization"));
+
+  if (!userId) {
+    userId = ctx.request.ip;
+    await checkAnonReqCount(ctx.request.ip);
+  }
+
+  // @ts-ignore i dunno
+  const agentId = ctx.params.agentId;
+
+  const agent = await kv.get<AgentConfiguration>([AGENT_CONFIG, agentId]);
 
   ctx.response.body = agent.value;
 };
 
 interface AgentConfiguration {
+  userId: string;
   name: string;
   description: string;
   systemMessage?: string;
@@ -40,8 +60,11 @@ export const createAgent = async (ctx: Context) => {
       await checkAnonReqCount(ctx.request.ip);
     }
 
+    const agentId = crypto.randomUUID();
+
     const body = await ctx.request.body.json();
     const configuration = {
+      userId,
       name: body.name,
       description: body.description,
       systemMessage: body.systemMessage,
@@ -49,12 +72,12 @@ export const createAgent = async (ctx: Context) => {
       createdAt: Date.now(),
     } satisfies AgentConfiguration;
 
-    const response = await kv.set(getMainConfigKey(userId), configuration);
+    await kv.set([AGENT_CONFIG, agentId], configuration);
 
-    if (!response.ok) throw new Error("Failed to create agent");
+    await kv.set([USER_AGENTS, userId, agentId], true);
 
     ctx.response.status = Status.Created;
-    ctx.response.body = response;
+    ctx.response.body = agentId;
   } catch (error) {
     console.error("Unable to save agent configuration", error);
 
